c2210f45a03e080710083d433a0d82c0185bb5a1
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2025-04-02 16:29:19 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,28 @@
 pipeline {
     agent any
+    parameters {
+        choice(name: 'ENV', choices: ['dev', 'qa', 'prod'], description: 'Select the environment to deploy')
+    }
     tools {
         jdk 'jdk17'
         maven 'maven'
+    }
+    environment {
+        APP_NAME = 'my-app'
+        IMAGE_REPO = 'my-docker-repo'
     }
     stages {
         stage('git checkout') {
             steps {
                 git 'https://github.com/tirumalareddysanampudi/mvnproj.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                script {
+                    def tag = "${params.ENV}-${env.BUILD_NUMBER}"
+                    sh "docker build -t ${IMAGE_REPO}:${tag} ."
+                }
             }
         }
     }
