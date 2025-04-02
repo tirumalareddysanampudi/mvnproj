41e2f5c28e750cd1e46abf83be3eed7bdc78ef71
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2025-04-02 13:33:02 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,28 +1,14 @@
-pipeline{
-agent any
-environment{
- PATH="$MAVEN_PATH/bin:$PATH"
-}
-stages{
-stage('Git-Clone'){
- steps{
- git credentialsId: 'Git-Hub', url: 'https://github.com/tirumalareddysanampudi/mvnproj.git'
- }
-}
-stage('maven build'){
-steps{
- sh "mvn clean validate "
-}
-}
-stage('maven-compile'){
-steps{
- sh "mvn compile"
-}
-}
-stage('Maven package'){
-        steps{
-         sh "mvn package"
+pipeline {
+    agent any
+    tools {
+        jdk 'jdk17'
+        maven 'maven'
+    }
+    stages {
+        stage('git checkout') {
+            steps {
+                git 'https://github.com/tirumalareddysanampudi/mvnproj.git'
+            }
         }
     }
-}
 }
